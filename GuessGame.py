2c1d90afe6d80471174a2c4400e4e3f557609044
undevcover <ronkeghali@gmail.com>
2022-12-31 15:11:14 +0100

    Source code of Guess Game

---
+++ GuessGame.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import random
+
+def guessGame(limit):
+    count = 0
+    compGuess = random.randint(1, limit)
+    myGuess = 0;
+    while(myGuess != compGuess):
+        count = count + 1
+        myGuess = eval(input("Enter your guess: "))
+        if (compGuess > myGuess):
+            print("Too Low, try again")
+        elif (compGuess < myGuess):
+            print("Too high, guess lower")
+
+    print(f"Yaay! you guessed it right after {count} attempt(s)")
+
+
+guessGame(10)
